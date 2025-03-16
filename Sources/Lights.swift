d21fe9d4807381780bf92a79711b950b83ad3a67
Alex Hamlin <alex@alexhamlin.co>
2025-03-16 14:16:20 -0700

    Follow https://www.swift.org/getting-started/cli-swiftpm/

---
+++ Sources/Lights.swift
@@ -0,0 +1,15 @@
+//
+//  Lights.swift
+//  Lights
+//
+//  Created by Alex Hamlin on 3/16/25.
+//
+
+import Figlet
+
+@main
+struct FigletTool{
+    static func main() {
+        Figlet.say("Hello, Lights!")
+    }
+}
